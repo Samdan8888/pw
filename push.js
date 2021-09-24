3582eca583dd54e43e3ef4f50a358fffc53e9cde
Samdan <samdanfb1@gmail.com>
2021-09-25 00:02:50 +0800

    initial

---
+++ push.js
@@ -0,0 +1,5 @@
+var push = require('web-push');
+let vapidKeys {
+    publicKey: 'BGQCSniAe_edgny0Ue0KVvfCCvBYtqMkIObax6O88DsOhPr8IxYYqmzcj3HPjcy_yGBFziroS87XqDGCbdhFOrI', 
+    privateKey: 'nlopnF6wbAiHdb3FE_UqucLwbCu6U2G_c2xOo_-8LQc'
+}
